e021aa3abda569166fbccd97167d4f3d4bafe940
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-01-19 18:17:29 +0530

    New file added four

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1       
+PERSON2=$2
+
+echo "$PERSON1::How are you?"
+echo "$PERSON2::I'm good, How are you?"
+echo "$PERSON1::What are you doing?"
+echo "$PERSON2::I'm Working as an Devops engineer"
