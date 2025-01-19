b7973ce2090adb992fc50ba5eca00d47c28dd22b
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-01-19 18:07:58 +0530

    New file three addedd with conversation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Abhi        
+PERSON2=Nishu
+
+echo "$PERSON1::How are you?"
+echo "$PERSON2::I'm good, How are you?"
+echo "$PERSON1::What are you doing?"
+echo "$PERSON2::I'm Working as an Devops engineer"
